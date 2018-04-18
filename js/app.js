297deacff9ed6307490b02ec88061c751fd7aaf5
Jinesh Patel <jinesh701@gmail.com>
2018-04-18 08:29:50 -0400

    added underlining of current page

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ function showHomePage() {
     event.preventDefault();
     $('.about-me-section').hide();
     $('.projects-section').hide();
+    $('.home-link').addClass('current-page');
+    $('.projects-link').removeClass('current-page');
+    $('.about-link').removeClass('current-page');
     $('.home-page-section').fadeIn('slow');
   });
 }
@@ -12,6 +15,9 @@ function showAboutMe() {
     event.preventDefault();
     $('.home-page-section').hide();
     $('.projects-section').hide();
+    $('.about-link').addClass('current-page');
+    $('.home-link').removeClass('current-page');
+    $('.projects-link').removeClass('current-page');
     $('.about-me-section').fadeIn('slow');
   });
 }
@@ -21,6 +27,9 @@ function showProjects() {
     event.preventDefault();
     $('.home-page-section').hide();
     $('.about-me-section').hide();
+    $('.projects-link').addClass('current-page');
+    $('.about-link').removeClass('current-page');
+    $('.home-link').removeClass('current-page');
     $('.projects-section').fadeIn('slow');
   });
 }
